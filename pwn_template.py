71329904deb25c90629b8edbd03f7f882b3047de
popjy0312 <popjy0312@naver.com>
2024-12-06 19:29:55 -0800

    fix: add pie bp example and split binary name and image, container name

--- pwn_template.py
+++ pwn_template.py
@@ -1,37 +1,43 @@
 from pwn import *
 
-binary = 'iofile_vtable_check'
+
+binary = 'prob'
+image = 'ultrac'
+container = 'ultrac'
+
 e = ELF(binary)
-libc = './libc.so.6'
-l = ELF(libc)
+# libc = './libc.so.6'
+# l = ELF(libc)
 
 context.binary = e
 context.log_level = 'debug'
 # context.log_level = 'info'
 context.terminal = ['tmux', 'splitw', '-h']
-
+context.aslr = False
 
 HOST, PORT = '127.0.0.1 12999'.split()
 GDBSCRIPT = '\n'.join([
+    # f'b *$_base("{binary}")+0x1030' # pie breakpoint sample
 ])
 
 if args.REMOTE:
     p = remote('host3.dreamhack.games', 8267)
 elif args.SET:
+    LPORT=8080
     run_cmd = ("docker run "
                "--cap-add=SYS_PTRACE " 
                "--security-opt "
                "seccomp=unconfined "
                "--privileged "
                "-d "
-               f"-p {PORT}:{PORT} --name {binary} {binary}")
+               f"-p {PORT}:{LPORT} --name {container} {image}")
     info(run_cmd)
     subprocess.run(run_cmd.split(), shell=False)
     exit(0)
 elif args.GDB:
     p = remote(HOST, PORT)
     pid = int(subprocess.check_output(
-            f"docker top {binary} -eo pid,comm | tail -n 1 | awk '{{print $1}}'", 
+            f"docker top {container} -eo pid,comm | tail -n 1 | awk '{{print $1}}'", 
             shell=True
         ).decode().strip().split()[-1])
     rootfs = f"/proc/{pid}/root"
